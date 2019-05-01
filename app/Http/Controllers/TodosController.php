98000315fc4f8dfc0b77f9e48d0f912de2a9d429
Felipe <fae.20@hotmail.com>
2019-04-30 22:35:34 -0300

    displaying a single todo

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -8,8 +8,13 @@ use Illuminate\Http\Request;
 
 class TodosController extends Controller
 {
-    public function index(){
-
+    public function index()
+    {
         return view('todos.index')->with('todos', Todo::all());
+    }
+
+    public function show($todoId)
+    {
+        return view('todos.show')->with('todo', Todo::find($todoId));
     }
 }
