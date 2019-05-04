6c3315b90cc8aaa01a2979accea579731abb4aac
Felipe <fae.20@hotmail.com>
2019-05-04 10:33:48 -0300

    Creating todos

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('todos', 'TodosController@index');
 
 Route::get('todos/{todo}', 'TodosController@show');
+
+Route::get('new-todos', 'TodosController@create');
+
+Route::post('store-todos', 'TodosController@store');
